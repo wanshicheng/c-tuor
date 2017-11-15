7a3170e7532ce062cdb8a7272add5856b6f7817c
Timothy Wan <wanshicheng@qq.com>
2017-11-15 17:37:13 +0800

    CMD

---
+++ CMD/CMD-Test/main.cpp
@@ -0,0 +1,9 @@
+// 被设为启动项目
+#include<stdlib.h>
+
+void main()
+{
+	// system("ipconfig");
+	system("D:\\workspace\\c\\c-tuor\\CMD\\Debug\\CMD.exe"); //调用自己写的exe文件
+	system("pause");
+}
---
+++ CMD/CMD/main.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+void main()
+{
+	printf("锄禾日当午，写C真是苦！");
+	getchar();
+}
