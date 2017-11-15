8f5ea4358103647ae9f43d72ac9a9f434a56a49b
Timothy Wan <wanshicheng@qq.com>
2017-11-15 17:01:28 +0800

    Hello World!

---
+++ HelloWorld/HelloWorld/helloworld.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h> // C语言的输入输出靠标准库
+
+/*
+注释多行
+*/
+void main()
+{
+	/*
+	printf("hello world!");
+	
+	printf("你好，世界！");
+	*/
+
+	/*
+	#和include之间可以有多个空格。告诉编译器在编译之前要做预先处理。
+	#include<file>编译程序会先到标准函数库中找文件。
+	#include"file"编译程序会先从当前目录中找文件。
+	*/
+	#include"helloworld.h"
+	#include"helloworld.wsc"
+	getchar(); // 等待输入一个字符，获取字符后退出
+}
